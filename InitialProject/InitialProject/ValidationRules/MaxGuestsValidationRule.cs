a2de946b377d31e5d643a93da5b73c7c3b0b7b88
sirarmilos <losmac256@gmail.com>
2023-03-12 23:24:20 +0100

    commit_with_url
    Accommodation registration with url, without validations

---
+++ InitialProject/InitialProject/ValidationRules/MaxGuestsValidationRule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace InitialProject.ValidationRules
+{
+    public class MaxGuestsValidationRule : ValidationRule
+    {
+        public int GreatherThenZero
+        {
+            get; set;
+        }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (Convert.ToInt32(value) <= 0)
+            {
+                return new ValidationResult(false, "Max Numbers must be greather then zero.");
+            }
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
