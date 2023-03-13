55e9cc4e89910f8b2cb9055b874c265889d3e6f5
Vasilije <zekovic.in4.2020@uns.ac.rs>
2023-03-13 22:27:20 +0100

    commit_without_validations

--- InitialProject/InitialProject/Repository/AccommodationRepository.cs
+++ InitialProject/InitialProject/Repository/AccommodationRepository.cs
@@ -29,6 +29,20 @@ namespace InitialProject.Repository
             accommodations = accommodationSerializer.FromCSV(FilePathAccommodation);
             locationSerializer = new Serializer<Location>();
             locations = locationSerializer.FromCSV(FilePathLocation);
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (locations == null)
+                    break;
+                foreach (Location location in locations)
+                {
+                    if (location.Id == accommodation.Location.Id)
+                    {
+                        accommodation.Location = location;
+                        break;
+                    }
+                }
+            }
         }
 
         public void Save(string accommodationName, string country, string city, string address, decimal latitude, decimal longitude, string type, int maxGuests, int minDaysReservation, int leftCancelationDays, List<string> images)
@@ -83,6 +97,186 @@ namespace InitialProject.Repository
             }
             return locations.Max(c => c.Id) + 1;
         }
+
+
+        // null,blank and white-spaced arguments will be ignored
+        public List<Accommodation> FindAll(string accommodationName, string country, string city, string type, int maxGuests, int minDaysReservation)
+        {
+            List<Accommodation> accommodationNameResults, countryResults, cityResults, typeResults, maxGuestsResults, minDaysReservationResults;
+
+            if (!string.IsNullOrWhiteSpace(accommodationName))
+            {
+                accommodationName = accommodationName.Trim();
+                accommodationNameResults = FindAllByAccommodation(accommodationName);
+                if (accommodationNameResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                accommodationNameResults = accommodations;
+            }
+
+
+            // todo: proveriti ovo
+            if (!string.IsNullOrWhiteSpace(country))
+            {
+                country = country.Trim();
+                countryResults = FindAllByCountry(country);
+                if (countryResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                countryResults = accommodations;
+            }
+
+
+            // todo: proveriti ovo
+            if (!string.IsNullOrWhiteSpace(city))
+            {
+                city = city.Trim();
+                cityResults = FindAllByCity(city);
+                if (cityResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                cityResults = accommodations;
+            }
+
+
+
+            if (!string.IsNullOrWhiteSpace(type))
+            {
+                type = type.Trim();
+                typeResults = FindAllByType(type);
+                if (typeResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                typeResults = accommodations;
+            }
+
+
+
+            if (maxGuests > 0)
+            {
+                maxGuestsResults = FindAllByMaxGuestsNumber(maxGuests);
+                if (maxGuestsResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                maxGuestsResults = accommodations;
+            }
+
+
+            if (minDaysReservation > 0)
+            {
+                minDaysReservationResults = FindAllAboveMinReservationDays(minDaysReservation);
+                if (minDaysReservationResults.Count == 0)
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                minDaysReservationResults = accommodations;
+            }
+
+            return accommodationNameResults.Intersect(cityResults).Intersect(countryResults).Intersect(typeResults).Intersect(maxGuestsResults).Intersect(minDaysReservationResults).ToList();
+        }
+
+        public List<Accommodation> FindAllByAccommodation(string name)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.AccommodationName.ToLower().StartsWith(name.ToLower()))
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+        public List<Accommodation> FindAllByCountry(string name)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.Location.Country.ToLower().StartsWith(name.ToLower()))
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+        public List<Accommodation> FindAllByCity(string name)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.Location.City.ToLower().StartsWith(name.ToLower()))
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+        public List<Accommodation> FindAllByMaxGuestsNumber(int quantity)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.MaxGuests >= quantity)
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+        public List<Accommodation> FindAllByType(string name)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.Type.ToLower().Equals(name.ToLower()))
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+        public List<Accommodation> FindAllAboveMinReservationDays(int minDaysReservation)
+        {
+            List<Accommodation> searchResult = new List<Accommodation>();
+
+            foreach (Accommodation accommodation in accommodations)
+            {
+                if (accommodation.MinDaysReservation <= minDaysReservation)
+                    searchResult.Add(accommodation);
+            }
+
+            return searchResult;
+        }
+
+
 
     }
 }
---
+++ InitialProject/InitialProject/View/SearchAndShowAccommodation.xaml.cs
@@ -0,0 +1,226 @@
+﻿using InitialProject.Model;
+using InitialProject.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace InitialProject.View
+{
+    /// <summary>
+    /// Interaction logic for SearchAndShowAccommodation.xaml
+    /// </summary>
+    public partial class SearchAndShowAccommodation : Window
+    {
+        public Accommodation Accommodation { get; set; }
+
+        private AccommodationRepository accommodationRepository;
+
+        private string accommodationName;
+        private string country;
+        private string city;
+        private string type;
+        private int maxGuests;
+        private int minDaysReservation;
+        private int leftCancelationDays;
+        private string image;
+        private List<string> images;
+
+        public string Image
+        {
+            get { return image; }
+            set
+            {
+                image = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> Images
+        {
+            get;
+            set;
+        }
+
+        public string AccommodationName
+        {
+            get { return accommodationName; }
+            set
+            {
+                accommodationName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                OnPropertyChanged(nameof(Country));
+                //Validate();
+
+            }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+                OnPropertyChanged(nameof(City));
+                //Validate();
+            }
+        }
+
+        public int MaxGuests
+        {
+            get { return maxGuests; }
+            set
+            {
+                maxGuests = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int MinDaysReservation
+        {
+            get { return minDaysReservation; }
+            set
+            {
+                minDaysReservation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int LeftCancelationDays
+        {
+            get { return leftCancelationDays; }
+            set
+            {
+                leftCancelationDays = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool apartment;
+        public bool Apartment
+        {
+            get { return apartment; }
+            set
+            {
+                if (value != apartment)
+                {
+                    apartment = value;
+                    OnPropertyChanged(nameof(apartment));
+                    if (value) Type = "Apartment";
+                }
+            }
+        }
+
+        private bool home;
+        public bool Home
+        {
+            get { return home; }
+            set
+            {
+                if (value != home)
+                {
+                    home = value;
+                    OnPropertyChanged(nameof(Home));
+                    if (value) Type = "Home";
+                }
+            }
+        }
+
+        private bool hut;
+        public bool Hut
+        {
+            get { return hut; }
+            set
+            {
+                if (value != hut)
+                {
+                    hut = value;
+                    OnPropertyChanged(nameof(Hut));
+                    if (value) Type = "Hut";
+                }
+            }
+        }
+
+        public string Type
+        {
+            get { return type; }
+            set
+            {
+                if (value != type)
+                {
+                    type = value;
+                    OnPropertyChanged(nameof(Type));
+                }
+            }
+        }
+
+
+        /*public bool IsValid { get; private set; } = false;
+
+        private void Validate()
+        {
+            if (!string.IsNullOrEmpty(City) && string.IsNullOrEmpty(Country))
+            {
+                IsValid = false;
+            }
+            else if (string.IsNullOrEmpty(City) && !string.IsNullOrEmpty(Country))
+            {
+                IsValid = false;
+            }
+            else
+            {
+                IsValid = true;
+            }
+        }*/
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        public SearchAndShowAccommodation()
+        {
+            InitializeComponent();
+            DataContext = this;
+            accommodationRepository = new AccommodationRepository();
+            Images = new List<string>();
+        }
+
+
+        private void Search(object sender, RoutedEventArgs e)
+        {
+            listAccommodations.ItemsSource = accommodationRepository.FindAll(AccommodationName, Country, City, Type, MaxGuests, MinDaysReservation);
+        }
+
+       
+    }
+
+}
