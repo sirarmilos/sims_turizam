fa013cdaf3e5a5ee9de3fc59a6d5ed18edb53ce6
in15 <103326160+Andrejj23@users.noreply.github.com>
2023-03-26 20:29:26 +0200

    creating_model_for_tourGuests

---
+++ InitialProject/InitialProject/Model/GuestOnTour.cs
@@ -0,0 +1,51 @@
+﻿using InitialProject.Repository;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+    public class GuestOnTour : ISerializable
+    {
+        public int Id { get; set; }
+        public TourGuest? Guest { get; set; }
+        public TourKeyPoint? TourKeyPoint { get; set; }
+
+        public GuestOnTour() { }
+
+        public GuestOnTour(int id, TourGuest guest, TourKeyPoint tourKeyPoint)
+        {
+            Id = id;
+            Guest = guest;
+            TourKeyPoint = tourKeyPoint;
+        }
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Guest.Id.ToString(), TourKeyPoint.Id.ToString() };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+
+            TourGuestRepository tourGuestRepository = new TourGuestRepository();
+            TourGuest guest = tourGuestRepository.GetById(Convert.ToInt32(values[1]));
+            Guest = guest;
+
+            TourKeyPointRepository tourKeyPointRepository = new TourKeyPointRepository();
+            TourKeyPoint tourKeyPoint = tourKeyPointRepository.GetById(Convert.ToInt32(values[2]));
+            TourKeyPoint = tourKeyPoint;
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ InitialProject/InitialProject/Model/TourGuest.cs
@@ -0,0 +1,39 @@
+﻿using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Model
+{
+    public class TourGuest : ISerializable
+    {
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public TourGuest() { }
+
+        public TourGuest(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), FirstName, LastName };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            FirstName = values[1];
+            LastName = values[2];
+        }
+    }
+}
