a59a8827365454f67ee1670f4846c5677f2b016b
sirarmilos <losmac256@gmail.com>
2023-03-30 21:33:37 +0200

    commit_login_start
    Added initial login, added homepages for each role
    
    Co-Authored-By: Vasilijez <103242980+Vasilijez@users.noreply.github.com>

---
+++ InitialProject/InitialProject/View/Guest1Start.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InitialProject.View
+{
+    /// <summary>
+    /// Interaction logic for Guest1Start.xaml
+    /// </summary>
+    public partial class Guest1Start : Window
+    {
+        public Guest1Start()
+        {
+            InitializeComponent();
+        }
+
+        private void GoToSearchAndShowAccommodation(object sender, RoutedEventArgs e)
+        {
+            SearchAndShowAccommodations window = new SearchAndShowAccommodations();
+            window.Show();
+        }
+    }
+}
