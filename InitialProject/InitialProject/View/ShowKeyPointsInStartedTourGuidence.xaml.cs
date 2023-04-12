e97da55cb8cf7b1ef6a2c452b189818e9747dc76
in15 <103326160+Andrejj23@users.noreply.github.com>
2023-04-12 22:41:34 +0200

    feature_cancel_tour

--- InitialProject/InitialProject/View/ShowKeyPointsInStartedTourGuidence.xaml.cs
+++ InitialProject/InitialProject/View/ShowKeyPointsInStartedTourGuidence.xaml.cs
@@ -27,17 +27,22 @@ namespace InitialProject.View
         private readonly TourGuidenceRepository tourGuidenceRepository;
         public static ObservableCollection<TourKeyPoint> tourKeyPoints { get; set; }
 
-        private int GuidenceId;
+        public TourGuidence TourGuidence { get; set; }
 
-        public ShowKeyPointsInStartedTourGuidence(int tourGuidenceId)
+        public List<TourGuidence> TourGuidences;
+
+        
+
+        public ShowKeyPointsInStartedTourGuidence(TourGuidence guidence, List<TourGuidence> guidences)
         {
             InitializeComponent();
             DataContext = this;
             tourKeyPointRepository = new TourKeyPointRepository();
             tourGuidenceRepository = new TourGuidenceRepository();
-            tourKeyPoints = new ObservableCollection<TourKeyPoint>(tourKeyPointRepository.Load(tourGuidenceId));
+            tourKeyPoints = new ObservableCollection<TourKeyPoint>(tourKeyPointRepository.Load(guidence.Id));
             tourKeyPoints[0].Passed = true;
-            GuidenceId = tourGuidenceId;
+            TourGuidence = guidence;
+            this.TourGuidences = guidences;
         }
 
         private void SaveCheckedKeyPoints(object sender, RoutedEventArgs e)
@@ -45,7 +50,7 @@ namespace InitialProject.View
             tourKeyPointRepository.UpdateCheckedKeyPoints();
             if (tourKeyPoints[tourKeyPoints.Count - 1].Passed == true)
             {
-                tourGuidenceRepository.UpdateFinishedField(tourKeyPoints[0].TourGuidence.Id);
+                tourGuidenceRepository.UpdateFinishedField(TourGuidence.Id);
             }
             this.Close();
         }
@@ -53,9 +58,50 @@ namespace InitialProject.View
         private void MarkPresentGuests(object sender, RoutedEventArgs e)
         {
 
-            ShowGuestOnTourGuidence window = new ShowGuestOnTourGuidence(GuidenceId);
+            ShowGuestOnTourGuidence window = new ShowGuestOnTourGuidence(TourGuidence.Id);
             window.Show();
 
         }
+
+        private void StartTourGuidence(object sender, RoutedEventArgs e)
+        {
+            if (tourGuidenceRepository.CheckGuidencesForStart(TourGuidences))
+            {
+                MessageBox.Show("You already started another tour!");
+                this.Close();
+                ShowTourGuidences window = new();
+                window.Show();
+            }
+            else
+            {
+                tourGuidenceRepository.UpdateStartedField(TourGuidence.Id);
+                MessageBox.Show("Tour successfully started");
+                //ShowKeyPointsInStartedTourGuidence window = new(TourGuidence, TourGuidences);
+                this.Close();
+                //window.Show();
+                ShowTourGuidences window = new();
+                window.Show();
+            }
+
+        }
+
+        private void FinishTourGuidence(object sender, RoutedEventArgs e)
+        {
+            if(TourGuidence.Started == false)
+            {
+                MessageBox.Show("Start your tour first!!");
+            }
+            else
+            {
+                tourGuidenceRepository.UpdateFinishedField(TourGuidence.Id);
+                MessageBox.Show("Tour successfully finished");
+                this.Close();
+                ShowTourGuidences window = new();
+                window.Show();
+            }
+
+            
+        }
+
     }
 }
